45701b3e93054a4d1c60b461c37e8bc8ccb68880
ACER <prasannakatti1908@gmail.com>
2024-03-11 15:41:12 +0530

    resourceserver

--- src/main/java/com/resourceserver/springresourceserver/Configuration/SecurityConfig.java
+++ src/main/java/com/resourceserver/springresourceserver/Configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.resourceserver.springresourceserver.Configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +10,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(oauth->oauth.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authz -> authz.anyRequest()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
     }
 }
--- src/main/java/com/resourceserver/springresourceserver/Controller/ResourceServerController.java
+++ src/main/java/com/resourceserver/springresourceserver/Controller/ResourceServerController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/spring-resource-server")
 public class ResourceServerController {
 
     @GetMapping("/home")
